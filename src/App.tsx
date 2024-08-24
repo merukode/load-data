0aeaf3f583732760ecefa4bfd849a3d58eab7ae0
merukode <auliarama965@gmail.com>
2024-08-24 08:56:51 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import {Button} from "./components/ui/button";
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Button>Click Me</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
